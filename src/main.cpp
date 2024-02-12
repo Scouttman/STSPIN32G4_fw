fa86b19f9bc1ab6994cc341849a2b8e525aeb084
scouttman <sctt.frsr@gmail.com>
2024-02-12 20:32:07 +1100

    using the simplefoc driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,23 @@
 #include <Arduino.h>
-#include "main.h"
-#include "pin.h"
-#include "stspin32g4.h"
-#define _STM32_DEF_ // this shoudn't be needed
-#include <SimpleFOC.h>
+#include "SimpleFOC.h"
+#include "SimpleFOCDrivers.h"
+#include "drivers/stspin32g4/STSPIN32G4.h"
 
-#ifdef SIMPLEFOC_H
-// BLDCMotor motor = BLDCMotor(11); // for xing
-// BLDCMotor motor = BLDCMotor(7); // for gartt
-BLDCMotor motor = BLDCMotor(7); // for drive module
+#define LED_WHITE PB_10 
+#define LED_RED PC_13
 
-// BLDC driver instance
-BLDCDriver6PWM driver = BLDCDriver6PWM(INH1,INL1,INH2,INL2,INH3,INL3);
-// BLDCDriver6PWM driver = BLDCDriver6PWM(LED_YELLOW,INL1,INH2,INL2,INH3,INL3);
-// int phA_h,int phA_l,int phB_h,int phB_l,int phC_h,int phC_l, int en = NOT_SET
-float target_velocity = 10;
-#endif
+STSPIN32G4 driver = STSPIN32G4();
+BLDCMotor motor = BLDCMotor(7);
 
-static void MX_I2C3_Init(void);
+Encoder encoder = Encoder(PB5, PB4, 2048, PB6);
 
-I2C_HandleTypeDef hi2c3;
-STSPIN32G4_HandleTypeDef HdlSTSPING4;
-TIM_HandleTypeDef htim1;
-TIM_HandleTypeDef htim2;
-
-Encoder encoder = Encoder(PB5, PB4, 2048, PB3); //AMT
-// Encoder encoder = Encoder(PB5, PB4, 5000, PB3);
 // interrupt routine intialisation
 void doA(){encoder.handleA();}
 void doB(){encoder.handleB();}
 void doI(){encoder.handleIndex();}
+
 unsigned long long startTime;
-unsigned int counter = 0;
+unsigned int counter;
 
 void setup() {
   Serial.begin(115200);
@@ -45,14 +31,6 @@ void setup() {
   /* Initialize all configured peripherals */
   pinMode(LED_WHITE, OUTPUT);
   pinMode(LED_RED, OUTPUT);
-  // digitalWrite(LED_WHITE, HIGH);
-  // analogWriteFrequency(12000);
-  analogWrite(LED_WHITE, LOW);
-  digitalWrite(LED_RED, LOW);
-
-  motor.PID_velocity.P = 0.1f;
-  motor.PID_velocity.I = 30;
-  motor.PID_velocity.D = 0;
 
   // enable/disable quadrature mode
   encoder.quadrature = Quadrature::ON;
@@ -62,32 +40,23 @@ void setup() {
   encoder.enableInterrupts(doA, doB, doI);
   motor.linkSensor(&encoder);
 
-  //   /* USER CODE BEGIN 2 */
-  // pwm frequency to be used [Hz]
-  // for atmega328 fixed to 32kHz
-  // esp32/stm32/teensy configurable
-  driver.pwm_frequency = 32000;
-  // power supply voltage [V]
-  driver.voltage_power_supply = 24;
-  // Max DC voltage allowed - default voltage_power_supply
-  driver.voltage_limit = 8.0; //>3 breaks things?1.5;//5.0;
-  // dead_zone [0,1] - default 0.02f - 2%
-  driver.dead_zone = 0.05f;
-
-  // driver init
-  if(!driver.init()){
-    // Error_Handler();
-  }
-
-  setupDriver();
+  driver.voltage_power_supply = 24.0f;
+  driver.voltage_limit = 1.0f;
+  driver.init();
+  motor.voltage_limit = driver.voltage_limit / 2.0f;
+  // motor.controller = MotionControlType::velocity;
+  // set the torque control type
+  motor.phase_resistance = 12.5; // 12.5 Ohms
+  motor.torque_controller = TorqueControlType::voltage;
+  // set motion control loop to be used
+  motor.controller = MotionControlType::torque;
   motor.linkDriver(&driver);
-  motor.voltage_limit = 4;
-  motor.voltage_sensor_align = 1;
+  motor.init();
 
-  // 
-  motor.controller = MotionControlType::velocity;
-  
-  // driver.voltage_limit = 0.5; // lower voltage for homing
+  motor.PID_velocity.P = 0.1f;
+  motor.PID_velocity.I = 0;
+  motor.PID_velocity.D = 0;
+
   // initialize motor
   motor.init();
   // align encoder and start FOC
@@ -101,120 +70,18 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_WHITE, !digitalRead(READY));
-  digitalWrite(LED_RED, !digitalRead(NFAULT));
-  if(!digitalRead(NFAULT)){
-    Serial.print("Error:");
-    STSPIN32G4_statusRegTypeDef statusReg;
-	  STSPIN32G4_getStatus(&HdlSTSPING4, &statusReg);
-    // Serial.println((int)statusReg, BIN);
-    if(statusReg.vccUvlo){
-      Serial.println("VCC UVLO (voltage to low)");
-    }
-    if(statusReg.thsd){
-      Serial.println("voltage regulator thermal shutdown");
-    }
-    if(statusReg.vdsp){
-      Serial.println("Overvoltage protection");
-    }
-    if(statusReg.reset){
-      Serial.println("the device performed a reset or power up\n this could mean a brown out occurred");
-    }
-  }
-
+  // main FOC algorithm function
   motor.loopFOC();
-  // driver.setPwm(1,1,1);
-  // delay(100);
-  motor.move(100*_2PI);
-
-  if(millis() - startTime > 10000){
-    motor.disable();
+  motor.move(1.0f); // 5 rad/s open loop
+  delayMicroseconds(100); // STM32G4 is very fast, add a delay in open loop if we do nothing else
+  if(driver.isFault()){
+    digitalWrite(LED_WHITE, HIGH);
+    digitalWrite(LED_RED, HIGH);
+  }else{
+    digitalWrite(LED_WHITE, LOW);
+    digitalWrite(LED_RED, LOW);
   }
-  
-  if(counter%10000==1){
-    // display the angle and the angular velocity to the terminal
-    // Serial.println(encoder.getAngle());
-    Serial.println(encoder.getVelocity()/_2PI);
-    // Serial.println() #TODO print voltage level
+  if(counter%100){
+    Serial.println(motor.voltage.d);
   }
-  counter++;
 }
-
-void setupDriver(){
-  pinMode(WAKE, OUTPUT);
-  pinMode(READY, INPUT_PULLUP);
-  pinMode(NFAULT, INPUT_PULLUP);
-  digitalWrite(WAKE, HIGH); // STSPIN init should set this high but why not
-
-  /*************************************************/
-  /*   STSPIN32G4 driver component initialization  */
-  /*************************************************/
-  MX_I2C3_Init();
-  STSPIN32G4_init( &HdlSTSPING4 );
-  // Purge registers to default values
-  if(STSPIN32G4_reset(&HdlSTSPING4) != STSPIN32G4_OK){
-    Error_Handler();
-  }
-
-  // Disable the Buck regulator and demap VCC UVLO on NFAULT pin
-  STSPIN32G4_confVCC blah;
-  STSPIN32G4_confVCC vcc = {.voltage = blah._EXT, .useNFAULT=true, .useREADY=false};
-  if(STSPIN32G4_setVCC(&HdlSTSPING4, vcc) != STSPIN32G4_OK){Error_Handler();}
-
-  // Configure VDSP protection with 4us deglitch time and map triggering on NFAULT pin
-  STSPIN32G4_confVDSP hmm;
-  STSPIN32G4_confVDSP vdsp = {.deglitchTime=hmm._6us, .useNFAULT=true};
-  if(STSPIN32G4_setVDSP(&HdlSTSPING4, vdsp) != STSPIN32G4_OK){Error_Handler();}
-
-  // After reset a clearing of fault is needed to enable GHSx/GLSx outputs
-  if(STSPIN32G4_clearFaults(&HdlSTSPING4) != STSPIN32G4_OK){Error_Handler();}
-}
-
-/**
-  * @brief I2C3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C3_Init(void)
-{
-
-  /* USER CODE BEGIN I2C3_Init 0 */
-
-  /* USER CODE END I2C3_Init 0 */
-
-  /* USER CODE BEGIN I2C3_Init 1 */
-
-  /* USER CODE END I2C3_Init 1 */
-  hi2c3.Instance = I2C3;
-  hi2c3.Init.Timing = 0x30A0A7FB;
-  hi2c3.Init.OwnAddress1 = 0;
-  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c3.Init.OwnAddress2 = 0;
-  hi2c3.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure Analogue filter
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure Digital filter
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C3_Init 2 */
-
-  /* USER CODE END I2C3_Init 2 */
-
-}
-
